482aa2a88bff8cac0d4da03d4825db6b70d4975f
Xu Xinran <xxr3376@gmail.com>
2014-11-23 21:27:53 +0800

    4th algorithm

--- m.cpp
+++ m.cpp
@@ -8,6 +8,7 @@
 #include <cstdio>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "HighAccuracy.h"
 #include "BITree.h"
 using std::string;
@@ -135,6 +136,76 @@ void m3(int argc, char **argv) {
     puts("");
 }
 
+void m4(int argc, char **argv) {
+    vector<int> p = read(argv[2]), q, p_;
+    int n = static_cast<int>(p.size());
+    p_.resize(n);
+    q.resize(n);
+    HighAccuracy k;
+    if (argc > 3)
+        k.load(argv[3]);
+    else
+        k = 1;
+
+    //step 1
+    for (int i = 0; i < n; ++i)
+        q[p[i] - 1] = i + 1;
+    BITree t(n, 0);
+    HighAccuracy m(0);
+    t.set(q[0], 1);
+    for (int i = 1, tmp = 0; i < n; ++i) {
+      int cur = (tmp&1) ?
+          t.sum(q[i] + 1, n) :
+          t.sum(1, q[i] - 1);
+
+      if (i&1) tmp = cur;
+      else tmp += cur;
+
+      m = m * (i + 1) + cur;
+      t.set(q[i], 1);
+    }
+    //step 2
+    HighAccuracy x = m + k;
+    //step 3
+    for (int i = n; i > 1; --i) {
+        q[i - 1] = x.div(i);
+    }
+    //step 4
+    t.reset(n, 1); //right
+    BITree t_reverse(n, 1); //left
+
+    for (int i = n - 1; i > 0; --i) {
+      int cur = 0;
+      if (i == 1)
+          cur = 0;
+      else if (i & 1)
+          cur = q[i-1] + q[i - 2];
+      else
+          cur = q[i-1];
+
+      int r;
+      if (cur & 1)
+          r = n - t_reverse.getKth(q[i] + 1);
+      else
+          r = t.getKth(q[i] + 1) - 1;
+
+      t.set(r+1, 0);
+      t_reverse.set(n - r, 0);
+
+      p_[r] = i + 1;
+    }
+
+    // put 1 back
+    p_[t.getKth(1) - 1] = 1;
+
+    for (int i = n - 1; i >= 0; --i)
+        printf("%d ", p_[i]);
+    puts("");
+
+
+
+}
+
 int main(int argc, char **argv) {
     if (argc < 3) {
         printf("usage: %s <type> <input permutation> [next]\n", argv[0]);
@@ -148,6 +219,7 @@ int main(int argc, char **argv) {
     } else if (string(argv[1]) == "3") {
         m3(argc, argv);
     } else if (string(argv[1]) == "4") {
+        m4(argc, argv);
     } else {
         printf("type: 1,2,3,4\n");
         return 0;
